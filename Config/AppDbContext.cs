9f12c7d1f4d8a048b3f23bc3cdf1419738146803
Jefferson Cruz <cruzjeff225@gmail.com>
2025-09-14 21:34:19 -0600

    feat: Adding Patient model

--- Config/AppDbContext.cs
+++ Config/AppDbContext.cs
@@ -1,9 +1,15 @@
 ﻿//References 
 using Microsoft.EntityFrameworkCore;
+using ClinicaAPI.Models;
 
 namespace ClinicaAPI.Config
 {
     public class AppDbContext : DbContext
     {
+        // Constructor for the DbContext
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+        public DbSet<Patient> Patients { get; set; }
     }
 }
---
+++ Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿namespace ClinicaAPI.Models
+{
+    public class Patient
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Genero { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaRegistro { get; set; }
+}
+}
